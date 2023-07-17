489445a0bfed69c8aef0ac4a44dd99d6bd8bcea9
secretariuss <secretarius@ukr.net>
2023-07-17 00:30:47 -0300

    add port const

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 const routes = require('./routes/index');
 
 app.use(express.static('public'));
